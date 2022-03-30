54761ecb638f1fe2fc13af44dc1181fc71b1e446
Mohamed Mahmoud <mohamedfci2018@gmail.com>
2022-03-30 03:37:20 +0200

    show in home & category

---
+++ src/app/Shared Classes/IProduct.ts
@@ -0,0 +1,12 @@
+export interface IProduct{
+    id:number,
+    name:string,
+    price:number,
+    descriptin:string,
+    quantity:number,
+    discount:number,
+    category:string,
+    type:string,
+    season:string,
+    firstImage:string
+}
