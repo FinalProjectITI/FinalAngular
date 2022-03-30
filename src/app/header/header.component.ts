a41327288b7c6a8586fd724a60b919a5474df702
Wessam Mustafa <52854472+Wessam-Mustafa@users.noreply.github.com>
2022-03-30 22:03:27 +0200

    new home

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {  CategoryService } from '../services/category.service';
+import { CategoryService } from '../services/category.service';
 import { ICategory } from '../Shared Classes/ICategory';
 
 @Component({
@@ -9,19 +9,21 @@ import { ICategory } from '../Shared Classes/ICategory';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  AllCategories?:ICategory[]
-  errmsg="";
-  constructor(private categorysevice: CategoryService,private router:Router) { }
+  AllCategories?: ICategory[]
+  errmsg = "";
+
+  constructor(private categorysevice: CategoryService, private router: Router) { }
 
   ngOnInit(): void {
     this.categorysevice.GetAllCategories().subscribe(
-      data=>{
+      data => {
         //console.log(data)
-        this.AllCategories=data},
-      error=>this.errmsg=error
+        this.AllCategories = data
+      },
+      error => this.errmsg = error
     );
   }
-  GetOneCategory(id:number){
-this.router.navigate(['category',id]);
+  GetOneCategory(id: number) {
+    this.router.navigate(['category', id]);
   }
 }
